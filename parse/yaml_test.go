1adf446afd98bcf181a6eaf0a4b344ea4ee0609a
Ilja Hämäläinen <ilja.hamalainen@gmail.com>
2018-11-10 20:55:38 +0200

    Add glide file and few tests

--- parse/yaml_test.go
+++ parse/yaml_test.go
@@ -3,6 +3,8 @@ package parse
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestParseFile(t *testing.T) {
@@ -25,22 +27,53 @@ func TestParseFile(t *testing.T) {
 	}
 }
 
-/*
 func TestOneCommandWithMessage(t *testing.T) {
 
 	// Given:
 	content := []byte(`
 commands:
-	/start:
-		message:
-		   en_gb: "Hello, this bot can help you to count clicks (CHANGE IT)"
-		   ru_ru: "Privet, etot bot mozet delatj raznye fishki (CHANGE IT)"
-	`)
+  /start:
+    message:
+      en_gb: "Hello, this bot can help you to count clicks (CHANGE IT)"
+      ru_ru: "Privet, etot bot mozet delatj raznye fishki (CHANGE IT)"`)
 
 	// When:
 	rootn, err := _parseYaml(content)
 
 	assert.Nil(t, err)
 	assert.NotNil(t, rootn)
+
+	// And:
+	assert.Equal(t, 1, len(rootn.Commands))
+	assert.Equal(t, "Hello, this bot can help you to count clicks (CHANGE IT)", rootn.Commands["/start"].Message["en_gb"])
+	assert.Equal(t, "Privet, etot bot mozet delatj raznye fishki (CHANGE IT)", rootn.Commands["/start"].Message["ru_ru"])
 }
-*/
+
+func TestFewCommandWithMessage(t *testing.T) {
+
+	// Given:
+	content := []byte(`
+commands:
+  /start:
+    message:
+      en_gb: "this is start"
+      ru_ru: "это начало"
+  /help:
+    message:
+      en_gb: "this is help"
+      ru_ru: "это помощь"`)
+
+	// When:
+	rootn, err := _parseYaml(content)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, rootn)
+
+	// and:
+	assert.Equal(t, 2, len(rootn.Commands))
+	assert.Equal(t, "this is start", rootn.Commands["/start"].Message["en_gb"])
+	assert.Equal(t, "это начало", rootn.Commands["/start"].Message["ru_ru"])
+
+	assert.Equal(t, "this is help", rootn.Commands["/help"].Message["en_gb"])
+	assert.Equal(t, "это помощь", rootn.Commands["/help"].Message["ru_ru"])
+}
